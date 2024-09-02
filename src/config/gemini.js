ce524382c9817f5a8023acec7e311fd47f30321f
Areeb <zahooriareeb@gmail.com>
2024-09-02 13:01:36 +0530

    env del

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI("AIzaSyCZy3PIV7vX_WiZvuDzjd2IkHl5twA9sfM");
 const model = genAI.getGenerativeModel({
 	model: "gemini-1.5-flash",
 });
@@ -9,4 +9,3 @@ export default async function runChat(input) {
 	const result = await model.generateContent(input);
 	return result.response.text();
 }
-
